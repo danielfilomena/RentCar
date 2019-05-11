d39011f9f8ea5cd45983077ad36966f1e8474a90
DanielSenac <48969151+DanielSenac@users.noreply.github.com>
2019-05-11 02:04:37 -0300

    Cruds - Cliente - Veículo e Usuários

---
+++ Veiculo.php
@@ -0,0 +1,17 @@
+<?php
+    class Veiculo {
+        public $id;
+        public $modelo;
+        public $marca;
+        public $ano;
+        public $placa;
+
+        function __construct($id, $modelo, $marca,$ano,$placa){
+            $this->id = $id;
+            $this->modelo = $modelo;
+            $this->marca = $marca;
+            $this->ano = $ano;
+            $this->placa = $placa;
+        }
+    }
+?>
