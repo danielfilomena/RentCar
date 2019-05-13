25bae1f154333a3bc3890e33ed023c276bdefbd4
Daniel Ricardo <daniel.ric@hotmail.com>
2019-05-13 18:24:25 -0300

    Correção crud Reserva.

--- RentCar/Trunk/APP/Models/ReservaVeiculo.php
+++ RentCar/Trunk/APP/Models/ReservaVeiculo.php
@@ -5,18 +5,16 @@
         public $id;
         public $modelo;
         public $marca;
-        public $placa;
         public $cliente;
         public $dataretirada;
         public $dataprevistaentrega;
         public $valor;
         public $formapagamento;
 
-        function __construct($id, $modelo, $marca,$placa,$cliente,$dataretirada,$dataprevistaentrega,$valor,$formapagamento){
+        function __construct($id, $modelo, $marca,$cliente,$dataretirada,$dataprevistaentrega,$valor,$formapagamento){
             $this->id = $id;
             $this->modelo = $modelo;
             $this->marca = $marca;
-            $this->placa = $placa;
             $this->cliente = $cliente;
             $this->dataretirada = $dataretirada;
             $this->dataprevistaentrega = $dataprevistaentrega;
